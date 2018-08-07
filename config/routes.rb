06b57e388a012a1741247410bea76ff9d4870fc4
Adam Sinning <asinning25@gmail.com>
2018-08-07 16:42:06 -0300

    wikinimous structure updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+get "articles", to: "articles#index"
+post "articles", to: "articles#create"
+
+get "articles/new", to: "articles#new"
+
+get "articles/:id", to: "articles#show", as: :article
+
+get "articles/:id/edit", to: "articles#edit", as: :edit
+patch  "articles/:id", to: "articles#update"
+
+delete "articles/:id", to: "articles#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
